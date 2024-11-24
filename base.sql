19673bf9331e68ad25b0a994452cb57134f272af
ismaworkdev <ismaelworkdv@gmail.com>
2024-11-24 23:35:09 +0100

    Base.sql

--- base.sql
+++ base.sql
@@ -1,48 +1,39 @@
-
-DROP TABLE usuarios CASCADE CONSTRAINTS;
-DROP TABLE vuelos CASCADE CONSTRAINTS;
-DROP TABLE viajes CASCADE CONSTRAINTS;
-
 CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT,
-    nombre VARCHAR2(50) PRIMARY KEY,
-    contraseña VARCHAR2(50),
-    rol VARCHAR2(20),
-    email VARCHAR2(100)
+    nombre VARCHAR(50) PRIMARY KEY,
+    contraseña VARCHAR(50),
+    rol VARCHAR(20),
+    email VARCHAR(100)
 );
 CREATE TABLE vuelos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    empresa VARCHAR2(50),
-    aeropuerto_origen VARCHAR2(100),
-    aeropuerto_destino VARCHAR2(100),
-    tiempo_estimado VARCHAR2(50),
+    empresa VARCHAR(50),
+    aeropuerto_origen VARCHAR(100),
+    aeropuerto_destino VARCHAR(100),
+    tiempo_estimado TIME,
     max_pasajeros INT CHECK (max_pasajeros <= 100),
-    precio VARCHAR2 (10, 2) ,
-    Fecha DATE,
+    precio DECIMAL(10, 2),
+    fecha DATE,
     hora TIME
 );
 CREATE TABLE viajes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_nombre VARCHAR(50),
     vuelo_id INT,
-   CONSTRAINT FOREIGN KEY (usuario_nombre) REFERENCES usuarios(nombre),
-  CONSTRAINT  FOREIGN KEY (vuelo_id) REFERENCES vuelos(id)
- 
+    PRIMARY KEY (usuario_nombre, vuelo_id),
+    CONSTRAINT FOREIGN KEY (usuario_nombre) REFERENCES usuarios(nombre),
+    CONSTRAINT FOREIGN KEY (vuelo_id) REFERENCES vuelos(id)
 );
-INSERT INTO usuarios (id, nombre, contraseña, rol, email) VALUES
-(1, 'admin', 'admin', 'A', 'admin.admin@gmail.com'),
-(2, 'juanperez', 'ahh', 'B', 'juan.perez@gmail.com'),
-(2, 'mariagomez', 'lol', 'B', 'maria.gomez@gmail.com'),
-(3, 'carloslopez', 'lol1', 'B', 'carlos.lopez@gmail.com');
 
 
-INSERT INTO vuelos (id, empresa, aeropuerto_origen, aeropuerto_destino, tiempo_estimado, max_pasajeros, precio , fecha , hora) VALUES
-(1, 'Iberia', 'Aeropuerto Adolfo Suàrez  Madrid-Barajas', 'Aeropuerto Josep Tarradellas Barcelona-El Prat', '02:00:00', 67, '60.00€', SYSDATE + 2, TO_DATE('7:14:00', 'HH24:MI:SS')),
-(2, 'Air Europa ', 'Aeropuerto Adolfo Suàrez  Madrid-Barajas', 'Aeropuerto Internacional El Dorado Bogotá, Colombia', '01:30:00', 50, '120.00€' , SYSDATE + 2 , TO_DATE('12:14:00', 'HH24:MI:SS')),
-(3, 'air France ', 'Aeropuerto Adolfo Suàrez  Madrid-Barajas', 'Aeropuerto Internacional Jorge Chávez Lima, Perú', '01:45:00', 40, '130.00€' , SYSDATE + 1 ,TO_DATE('10:14:00', 'HH24:MI:SS') );
-
-
-INSERT INTO viajes (id, usuario_nombre, vuelo_id) VALUES
-(1, 'juanperez', 1),
-(2, 'mariagomez', 2),
-(3, 'carloslopez', 3);
+INSERT INTO usuarios ( nombre, contraseña, rol, email) VALUES
+( 'admin', 'admin', 'A', 'admin.admin@gmail.com'),
+( 'juanperez', 'ahh', 'B', 'juan.perez@gmail.com'),
+( 'mariagomez', 'lol', 'B', 'maria.gomez@gmail.com'),
+( 'carloslopez', 'lol1', 'B', 'carlos.lopez@gmail.com');
+INSERT INTO vuelos (id, empresa, aeropuerto_origen, aeropuerto_destino, tiempo_estimado, max_pasajeros, precio, fecha, hora) VALUES
+(1, 'Iberia', 'Aeropuerto Adolfo Suárez Madrid-Barajas', 'Aeropuerto Josep Tarradellas Barcelona-El Prat', '02:00:00', 67, 60.00, '2024-10-22', '07:14:00'),
+(2, 'Air Europa', 'Aeropuerto Adolfo Suárez Madrid-Barajas', 'Aeropuerto Internacional El Dorado Bogotá, Colombia', '01:30:00', 50, 120.00, '2024-10-22', '12:14:00'),
+(3, 'Air France', 'Aeropuerto Adolfo Suárez Madrid-Barajas', 'Aeropuerto Internacional Jorge Chávez Lima, Perú', '01:45:00', 40, 130.00, '2024-10-21', '10:14:00');
+INSERT INTO viajes ( usuario_nombre, vuelo_id) VALUES
+( 'juanperez', 1),
+( 'mariagomez', 2),
+( 'carloslopez', 3);
