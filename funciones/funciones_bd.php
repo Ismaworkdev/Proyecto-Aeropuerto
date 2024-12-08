f820b3f8f1a3924a2a43f4994547c87fddfea70b
ismaworkdev <ismaelworkdv@gmail.com>
2024-12-08 20:00:59 +0100

    how to

--- funciones/funciones_bd.php
+++ funciones/funciones_bd.php
@@ -13,7 +13,7 @@ function conexion()
 {
     global $pdo;
     try {
-        $pdo = new PDO('mysql:host=localhost:3307;dbname=proyecto-aeropuerto', 'root', '');
+        $pdo = new PDO('mysql:host=localhost:3306;dbname=proyecto-aeropuerto', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->exec('SET NAMES "utf8"');
     } catch (PDOException $e) {
